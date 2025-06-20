6f4c180105563164ec877dad7cc970e702850335
thesaltysleuth <thesaltysleuth@gmail.com>
2025-06-20 12:41:26 +0530

    Structured response

---
+++ internal/api/response.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func respondJSON(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
+
+func respondError(w http.ResponseWriter, status int, message string) {
+	respondJSON(w, status, map[string]string{"error":message})
+}
+
